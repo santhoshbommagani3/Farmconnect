642029c031a0ab543a5521ce76b18232385e0cc1
B.Santhosh <santhoshbommagani3@gmail.com>
2026-06-20 13:17:07 +0530

    fix: inline postcss config in vite

--- farmconnect/client/vite.config.js
+++ farmconnect/client/vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-}) 
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+})
