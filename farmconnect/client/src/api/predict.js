6e71b7d10fd9dd1f547439ab71c6f19a4bbbe598
B.Santhosh <santhoshbommagani3@gmail.com>
2026-06-20 11:43:53 +0530

    update API URLs for deployment

--- farmconnect/client/src/api/predict.js
+++ farmconnect/client/src/api/predict.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
 export const predictPrice = async (state, commodity, variety, grade) => {
-  const response = await axios.post('http://localhost:5000/api/predict-price', {
-    state,
-    commodity,
-    variety,
-    grade
-  })
+  const response = await axios.post(
+    `${import.meta.env.VITE_API_URL || 'http://localhost:5000/api'}/predict-price`,
+    { state, commodity, variety, grade }
+  )
   return response.data
 }
