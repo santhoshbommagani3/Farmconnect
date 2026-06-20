59799e271692f25c603aed47d11b3edbe09760c8
B.Santhosh <santhoshbommagani3@gmail.com>
2026-06-20 11:13:30 +0530

    fix: correct Order model import casing

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import Order from '../models/order.js'
+import Order from '../models/Order.js'
 import protect from '../middleware/authMiddleware.js'
 
 const router = express.Router()
